073ee7bf6d84c4b171109606969b233c73731ba9
camillesulcova <cami12f4@easv365.dk>
2024-11-25 12:47:27 +0100

    Removing .php-suffix

--- masazoerdve/index.php
+++ masazoerdve/index.php
@@ -2,21 +2,21 @@
     <?php if(have_posts()): ?>
         <?php while(have_posts()): the_post() ?>
 <!--hero-->
-<?php get_template_part("template-parts/hero.php"); ?>
+<?php get_template_part("template-parts/hero"); ?>
 <!--hero-->
 
 
     <!--types of massages-->
-   <?php get_template_part ("template-parts/massages.php");?>
+   <?php get_template_part ("template-parts/massages");?>
   
     <!--types of massages-->
  <!--about me-->
- <?php get_template_part("template-parts/about.php"); ?>
+ <?php get_template_part("template-parts/about"); ?>
 
     <!--about me-->
 
     <!--benefits-->
-<?php get_template_part("template-parts/benefits.php"); ?>
+<?php get_template_part("template-parts/benefits"); ?>
     <!--benefits-->
 
 
