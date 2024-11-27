f1a56fd976feb4bb2a476131cb311ee108bbb6f8
MarinEASV <150655341+MarinEASV@users.noreply.github.com>
2024-11-27 09:46:03 +0100

    footer

--- masazoerdve/footer.php
+++ masazoerdve/footer.php
@@ -1,4 +1,64 @@
+    <!-- Footer Start -->
+<section id="footer">
+  <div class="container-fluid footer py-5" data-wow-delay="0.2s">
+    <div class="container py-5">
+      <div class="row g-5 justify-content-center text-center">
+        <div class="col-md-6 col-lg-6 col-xl-3">
+          <div class="footer-item d-flex flex-column">
+            <h4 class="text-black mb-4"><i class="fas fa-star-of-life me-3"></i>Terapia</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus dolorem impedit eos autem dolores
+              laudantium quia, qui similique
+            </p>
+            
+            <!-- Centered Social Media Section -->
+            <div class="social-icons d-flex justify-content-center align-items-center mt-4">
+              <!-- Share Icon -->
+              <i class="fas fa-share fa-2x me-3" style=" color: var(--c-purple);"></i>
 
+              <!-- Social Media Icons -->
+              <a class="btn-square btn text-white rounded-circle mx-1" href="#"
+                 style=" background-color: var(--c-purple); display: flex; align-items: center; justify-content: center; width: 30px; height: 30px;">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+
+              <a class="btn-square btn text-white rounded-circle mx-1" href="#"
+                 style=" background-color: var(--c-purple); display: flex; align-items: center; justify-content: center; width: 30px; height: 30px;">
+                <i class="fab fa-instagram"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+        <!-- Other sections remain unchanged -->
+        <div class="col-md-6 col-lg-6 col-xl-3">
+          <div class="footer-item d-flex flex-column">
+            <h4 class="mb-4 text-black">Quick Links</h4>
+            <a href=""><i class="fas fa-angle-right me-2"></i> About Us</a>
+            <a href=""><i class="fas fa-angle-right me-2"></i> Contact Us</a>
+            <a href=""><i class="fas fa-angle-right me-2"></i> Privacy Policy</a>
+            <a href=""><i class="fas fa-angle-right me-2"></i> Terms & Conditions</a>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-6 col-xl-3">
+          <div class="footer-item d-flex flex-column">
+            <h4 class="mb-4 text-black">Terapia Services</h4>
+            <a href=""><i class="fas fa-angle-right me-2"></i> All Services</a>
+            <a href=""><i class="fas fa-angle-right me-2"></i> Physiotherapy</a>
+            <a href=""><i class="fas fa-angle-right me-2"></i> Diagnostics</a>
+            <a href=""><i class="fas fa-angle-right me-2"></i> Manual Therapy</a>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-6 col-xl-3">
+          <div class="footer-item d-flex flex-column">
+            <h4 class="mb-4 text-black">Contact Info</h4>
+            <a href=""><i class="fa fa-map-marker-alt me-2"></i> 123 Street, New York, USA</a>
+            <a href=""><i class="fas fa-envelope me-2"></i> info@example.com</a>
+            <a href=""><i class="fas fa-phone me-2"></i> +012 345 67890</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 
         <?php wp_footer() ?>
     </body>
