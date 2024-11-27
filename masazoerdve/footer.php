184811059a2a648a89a942a7e0440de908e0b452
MarinEASV <150655341+MarinEASV@users.noreply.github.com>
2024-11-27 10:03:14 +0100

    footer info update

--- masazoerdve/footer.php
+++ masazoerdve/footer.php
@@ -6,9 +6,7 @@
         <div class="col-md-6 col-lg-6 col-xl-3">
           <div class="footer-item d-flex flex-column">
             <h4 class="text-black mb-4"><i class="fas fa-star-of-life me-3"></i>Terapia</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus dolorem impedit eos autem dolores
-              laudantium quia, qui similique
-            </p>
+            <p> All rights reserved © 2024 Masažo erdvė</p>
             
             <!-- Centered Social Media Section -->
             <div class="social-icons d-flex justify-content-center align-items-center mt-4">
@@ -16,12 +14,12 @@
               <i class="fas fa-share fa-2x me-3" style=" color: var(--c-purple);"></i>
 
               <!-- Social Media Icons -->
-              <a class="btn-square btn text-white rounded-circle mx-1" href="#"
+              <a class="btn-square btn text-white rounded-circle mx-1" href="https://www.facebook.com/kucinaitevelicke"
                  style=" background-color: var(--c-purple); display: flex; align-items: center; justify-content: center; width: 30px; height: 30px;">
                 <i class="fab fa-facebook-f"></i>
               </a>
 
-              <a class="btn-square btn text-white rounded-circle mx-1" href="#"
+              <a class="btn-square btn text-white rounded-circle mx-1" href="https://www.instagram.com/kucinaitevelicke/?igsh=anYzc2psbHc5OGdz"
                  style=" background-color: var(--c-purple); display: flex; align-items: center; justify-content: center; width: 30px; height: 30px;">
                 <i class="fab fa-instagram"></i>
               </a>
@@ -32,27 +30,25 @@
         <div class="col-md-6 col-lg-6 col-xl-3">
           <div class="footer-item d-flex flex-column">
             <h4 class="mb-4 text-black">Quick Links</h4>
-            <a href=""><i class="fas fa-angle-right me-2"></i> About Us</a>
-            <a href=""><i class="fas fa-angle-right me-2"></i> Contact Us</a>
+            <a href="#about"><i class="fas fa-angle-right me-2"></i> About Us</a>
+            <a href="#appointment"><i class="fas fa-angle-right me-2"></i> Contact Us</a>
             <a href=""><i class="fas fa-angle-right me-2"></i> Privacy Policy</a>
-            <a href=""><i class="fas fa-angle-right me-2"></i> Terms & Conditions</a>
           </div>
         </div>
         <div class="col-md-6 col-lg-6 col-xl-3">
           <div class="footer-item d-flex flex-column">
             <h4 class="mb-4 text-black">Terapia Services</h4>
-            <a href=""><i class="fas fa-angle-right me-2"></i> All Services</a>
-            <a href=""><i class="fas fa-angle-right me-2"></i> Physiotherapy</a>
-            <a href=""><i class="fas fa-angle-right me-2"></i> Diagnostics</a>
-            <a href=""><i class="fas fa-angle-right me-2"></i> Manual Therapy</a>
+            <a href="#massages"><i class="fas fa-angle-right me-2"></i> Physiotherapy</a>
+            <a href="#massages"><i class="fas fa-angle-right me-2"></i> Diagnostics</a>
+            <a href="#massages"><i class="fas fa-angle-right me-2"></i> Manual Therapy</a>
           </div>
         </div>
         <div class="col-md-6 col-lg-6 col-xl-3">
           <div class="footer-item d-flex flex-column">
             <h4 class="mb-4 text-black">Contact Info</h4>
             <a href=""><i class="fa fa-map-marker-alt me-2"></i> 123 Street, New York, USA</a>
-            <a href=""><i class="fas fa-envelope me-2"></i> info@example.com</a>
-            <a href=""><i class="fas fa-phone me-2"></i> +012 345 67890</a>
+            <a href=""><i class="fas fa-envelope me-2"></i>rita.masazoerdve@gmail.com</a>
+            <a href=""><i class="fas fa-phone me-2"></i>+37060715022</a>
           </div>
         </div>
       </div>
