b92af7c4f25c5c106076b1870eadf53e02491d28
MarinEASV <150655341+MarinEASV@users.noreply.github.com>
2024-11-27 12:37:20 +0100

    aos

--- masazoerdve/script.js
+++ masazoerdve/script.js
@@ -1,5 +1,7 @@
 <script>
-document.addEventListener('DOMContentLoaded', function() {
-    AOS.init();
-});
+  // Initialize AOS with a custom duration
+  AOS.init({{
+    duration: 1200,  // Duration of the animation in milliseconds (2 seconds)
+}});
 </script>
+
