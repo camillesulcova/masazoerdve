30adef3208084fff34b28ac03a42bf762d9827af
MarinEASV <150655341+MarinEASV@users.noreply.github.com>
2024-11-27 13:05:29 +0100

    footer update

--- masazoerdve/script.js
+++ masazoerdve/script.js
@@ -1,9 +1,8 @@
 <script>
 document.addEventListener('DOMContentLoaded', function() {
     AOS.init({
-        duration: 1200, // Animation duration in milliseconds
-        easing: 'ease-in-out', // Optional: Customize the easing function
-        once: true, // Optional: Whether animation should happen only once
+        duration: 1200, 
+       
     });
 });
 </script>
