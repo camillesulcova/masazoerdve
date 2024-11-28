90cd33b00d6362b31bf954ccd356cd66707701ad
Gudrute <150654963+Gudrute@users.noreply.github.com>
2024-11-28 15:16:18 +0100

    tad title

--- masazoerdve/header.php
+++ masazoerdve/header.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php bloginfo("name") ?></title>
-    <?php wp_head() ?>
-    <link rel="stylesheet" href="style.css">
+    <meta name="description" content="<?php bloginfo('description'); ?>">
+    <title><?php bloginfo('name'); ?></title>
+    <?php wp_head(); ?>
 </head>
+
     
 
 <body data-bs-spy="scroll" data-bs-target="#navScroll">
