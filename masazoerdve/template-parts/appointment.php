d938c7fabb7eefab1c9140d6d0243196f75bf014
MarinEASV <150655341+MarinEASV@users.noreply.github.com>
2024-11-28 12:29:49 +0100

    form

--- masazoerdve/template-parts/appointment.php
+++ masazoerdve/template-parts/appointment.php
@@ -54,11 +54,10 @@
         <!-- Right Block -->
         <div class="col-lg-6" data-aos="fade-left" data-aos-delay="500">
           <div class="appointment-form p-5 shadow" style="background-color: var(--c-light); box-shadow: 0px 4px 10px var(--c-pink);">
-          <?php echo do_shortcode('[contact-form-7 id="a78dfea" title="Book appoiment form"]'); ?>
-
             <h3 class="fs-4 text-uppercase mb-4" style="color: var(--c-dark);"><?php echo $title_modal_appointment; ?></h3>
             <h1 class="display-5 mb-4" style="color: var(--c-dark);"><?php echo $subtitle_modal_appointment; ?></h1>
             <!-- Insert the Contact Form 7 shortcode -->
+            <?php echo do_shortcode('[contact-form-7 id="a78dfea" title="Book appoiment form"]'); ?>
           </div>
         </div>
       </div>
