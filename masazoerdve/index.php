9789a3e39c3cebbd891d198d994b5c2a6d5ccfce
saresc01 <sara.escallon@gmail.com>
2024-11-28 20:50:10 +0100

    testimonials32

--- masazoerdve/index.php
+++ masazoerdve/index.php
@@ -25,7 +25,7 @@
     <!--testimonials-->
     <?php echo do_shortcode('[wp-testimonials widget-id=3]'); ?>
     <!--testimonials-->
-
+<br>
      <!-- Instagram -->
     <?php echo get_template_part("template-parts/instagram"); ?>
      <!-- Instagram End -->
