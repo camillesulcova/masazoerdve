600e84ed34a516c5e3511ae8000a167757673c7a
Gudrute <150654963+Gudrute@users.noreply.github.com>
2024-11-28 18:41:39 +0100

    Update hero.php

--- masazoerdve/template-parts/hero.php
+++ masazoerdve/template-parts/hero.php
@@ -11,11 +11,11 @@
       <div class="container">
         <div class="container position-relative">
           <div class="hero-image col-12 col-lg-8 mt-0 h-100 position-absolute top-0 end-0 bg-cover" data-aos="fade-left"
-            style="background-image: url(<?php echo $background_image["url"] ?>); background-size: contain; "></div>
+            style="background-image: url(<?php echo $background_image["url"] ?>); background-size: contain;"></div>
            
           <div class="row"> 
             <div class="col-lg-7 py-vh-6 position-relative" data-aos="fade-right">
-              <h1 class="display-1 mt-5" style="line-height: 1.2;"><?php echo $hero_title ?></h1>
+              <h1 class="display-1 mt-5" style="line-height: 1;"><?php echo $hero_title ?></h1>
               <br>
               <p class="lead"><?php echo $hero_text ?></p>
               <br>
