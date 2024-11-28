964d2424eebad9bb46a8886a885819d8d17d0081
Gudrute <150654963+Gudrute@users.noreply.github.com>
2024-11-28 17:40:40 +0100

    hero pic2

--- masazoerdve/template-parts/hero.php
+++ masazoerdve/template-parts/hero.php
@@ -10,9 +10,14 @@
     <section id="hero">
       <div class="container">
         <div class="container position-relative">
-          <div class="hero-image col-12 col-lg-8 mt-0 h-100 position-absolute top-0 end-0 bg-cover" data-aos="fade-left"
-            style="background-image: url(<?php echo $background_image["url"] ?>); background-size: contain;   background-repeat: no-repeat;"></div>
-           
+        <div style="background-image: url(<?php echo $background_image["url"]; ?>); 
+            background-size: contain; 
+            background-repeat: no-repeat; 
+            background-position: center; 
+            width: 100%; 
+            height: 500px;">
+        </div>
+
           <div class="row"> 
             <div class="col-lg-7 py-vh-6 position-relative" data-aos="fade-right">
               <h1 class="display-1 mt-5" style="line-height: 1.2;"><?php echo $hero_title ?></h1>
