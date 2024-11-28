5ff0ed2c440b746862ea49bc21c7ee16dcd0c626
saresc01 <sara.escallon@gmail.com>
2024-11-28 20:37:16 +0100

    icons benefits3

--- masazoerdve/template-parts/benefits.php
+++ masazoerdve/template-parts/benefits.php
@@ -40,9 +40,10 @@ $benefit_description = get_field("benefit_description");
 
             <div class="col-md-6 col-lg-4 border-end">
               <div class="py-3 text-start">
-                <img src="<?php echo $icon; ?>" width="42" height="auto" >
-                  
+                <img src="<?php echo $icon; ?>" width="42" height="auto" >                  
                 </img>
+
+                <img src="<?php echo $icon["url"]; ?>" class="icon" alt="<?php echo $icon; ?>">
 
                 <img src="<?php echo $icon; ?>" width="42" height="42" fill="currentColor" class="bi bi-box-seam mb-3" viewbox="0 0 16 16">
                   <path d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5l2.404.961L10.404 2l-2.218-.887zm3.564 1.426L5.596 5 8 5.961 14.154 3.5l-2.404-.961zm3.25 1.7-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923l6.5 2.6zM7.443.184a1.5.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464L7.443.184z" />
